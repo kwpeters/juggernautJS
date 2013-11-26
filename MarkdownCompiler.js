21e6adbd8b6f08e37c0198de8df3fd7757e904de
Kevin Peters <kwpeters@ra.rockwell.com>
2013-11-26 15:02:51 -0500

    Removed grunt dependency. Implemented md2html.js.

--- MarkdownCompiler.js
+++ MarkdownCompiler.js
@@ -1,12 +1,12 @@
 module.exports = (function () {
     'use strict';
 
-    var path  = require('path'),
+    var fs    = require('fs'),
+        path  = require('path'),
         rs    = require('robotskirt');
 
     // The constructor function that is exported from this module.
-    function MarkdownCompiler(grunt) {
-        this.grunt = grunt;
+    function MarkdownCompiler() {
     }
 
     // A function that will be used to override the HTML table rendering so we
@@ -55,7 +55,7 @@ module.exports = (function () {
         baseName = path.basename(sourceFile, extName);
         outputFile = path.join(dirName, baseName + '.html');
 
-        sourceFileContents = this.grunt.file.read(sourceFile);
+        sourceFileContents = fs.readFileSync(sourceFile, {encoding: 'utf8'});
         renderer = new rs.HtmlRenderer();
         renderer.table = renderTable;
 
@@ -66,56 +66,9 @@ module.exports = (function () {
         parser = new rs.Markdown(renderer, [rs.EXT_TABLES, rs.EXT_AUTOLINK,
             rs.EXT_FENCED_CODE, rs.EXT_STRIKETHROUGH]);
         outputFileContents = parser.render(sourceFileContents);
-        this.grunt.file.write(outputFile, outputFileContents);
+        fs.writeFileSync(outputFile, outputFileContents, {encoding: 'utf8'});
 
         callback(undefined, outputFile);
-    };
-
-
-    // Compiles a markdown file into a PDF document.
-    // sourceFile (string)   The path to the input markdown file
-    // callback(err, result) Callback that is called upon completion.
-    //                       If err is falsy, results will be the path to the
-    //                       output PDF file.
-    MarkdownCompiler.prototype.compileToPdf = function (sourceFile, callback) {
-        var sourceDirName,
-            sourceBaseName,
-            sourceExtName,
-            outputFile,
-            pdfTemplateFile,
-            pandocArgs,
-            pandocProcess;
-
-        pdfTemplateFile = path.join('..', 'pdf_template.tex');
-
-        sourceDirName = path.dirname(sourceFile);
-        sourceExtName = path.extname(sourceFile);
-        sourceBaseName = path.basename(sourceFile, sourceExtName);
-        outputFile = sourceBaseName + '.pdf';
-
-        pandocArgs = ['--template=' + pdfTemplateFile,
-                      '--latex-engine=xelatex',
-                      sourceBaseName + sourceExtName,
-                      '-o', outputFile];
-
-        pandocProcess = this.grunt.util.spawn(
-            {
-                cmd: 'pandoc',
-                args: pandocArgs,
-                opts: {cwd: sourceDirName}
-            },
-            function (error, result, code) {
-                if (error || (code !== 0)) {
-                    callback(
-                        new Error('Failed to generate ' + outputFile + ': ' +
-                                  String(result)),
-                        null);
-                } else {
-                    callback(undefined, path.join(sourceDirName, outputFile));
-                }
-
-            }
-        );
     };
 
     return MarkdownCompiler;
--- md2html.js
+++ md2html.js
@@ -1,6 +1,29 @@
 #!/usr/local/bin/node
 
 
-var mdCompiler = require('./MarkdownCompiler');
+var MarkdownCompiler = require('./MarkdownCompiler.js'),    // the constructor
+    mdCompiler = new MarkdownCompiler();              // a compiler instance
 
-console.log('hello world');
+
+function main() {
+    var filesToProcess = process.argv.slice(2),
+        curIndex,
+        curSourceFile;
+
+    for (curIndex = 0; curIndex < filesToProcess.length; ++curIndex) {
+
+        curSourceFile = filesToProcess[curIndex];
+
+        mdCompiler.compileToHtml(curSourceFile, function (err, results) {
+            if (err) {
+                //console.log(err);
+            } else {
+                // Successful compilation.
+                // The result is the output file name.
+                console.log('Successfully built ' + results);
+            }
+        });
+    }
+}
+
+main();

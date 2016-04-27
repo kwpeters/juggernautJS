2007c0583777b477c53085ef5f9b2bb94f80639d
Kevin Peters <kwpeters@ra.rockwell.com>
2016-04-26 20:45:57 -0400

    Implemented find.js.

--- src/find.js
+++ src/find.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
-var walk = require("walk"),
-    fs   = require("fs"),
-    path = require("path");
+var walk         = require("walk"),
+    fs           = require("fs"),
+    path         = require("path"),
+    chalk        = require("chalk"),
+    highlight    = chalk.bold.red;
+
+
+var pattern = new RegExp(process.argv[2]);
 
 
 var walker = walk.walk(process.cwd());
@@ -10,16 +15,27 @@ var walker = walk.walk(process.cwd());
 
 walker.on("file", function onFile(root, fileStats, next) {
     var absPath = path.join(root, fileStats.name);
-    console.log("file: " + absPath);
+    testName(absPath);
     next();
 });
 
 
 walker.on("directory", function onFile(root, dirStats, next) {
     var absPath = path.join(root, dirStats.name);
-    console.log("dir:  " + absPath);
+    testName(absPath);
     next();
 });
 
 
+function testName(name) {
 
+    var match = name.match(pattern);
+
+    if (match !== null) {
+        var before = match.input.slice(0, match.index);
+        var matching = match[0];
+        var after = match.input.slice(match.index + matching.length);
+
+        console.log(before + highlight(matching) + after);
+    }
+}

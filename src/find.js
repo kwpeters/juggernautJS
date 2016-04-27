eaec1b7c1748f6ad79031711dec58c2781528a2d
Kevin Peters <kwpeters@ra.rockwell.com>
2016-04-26 22:39:50 -0400

    Refactoring of find.js.

--- src/find.js
+++ src/find.js
@@ -1,36 +1,75 @@
 #!/usr/bin/env node
 
+var path = require("path"),
+    regexHelpers = require("./regexHelpers"),
+    chalk        = require("chalk"),
+    highlight    = chalk.bold.red;
+
+var filesystemRegex = new RegExp(process.argv[2], "i"),
+    contentsRegex = process.argv[3]? new RegExp(process.argv[3], "i") : null;
+
 doit();
 
 function doit() {
     "use strict";
 
     var walk         = require("walk"),
-        path         = require("path"),
-        regexHelpers = require("./regexHelpers"),
-        ignoreDirs   = [".git", ".idea"],
-        chalk        = require("chalk"),
-        highlight    = chalk.bold.red;;
+        ignoreDirs   = [".git", ".idea"];
 
-    var pattern = new RegExp(process.argv[2], "i");
+
 
     var walker = walk.walk(process.cwd(), {filters: ignoreDirs});
 
-    walker.on("file", function (root, fileStats, next) {
-        var absPath = path.join(root, fileStats.name);
-        var parts = regexHelpers.isMatch(absPath, pattern);
-        if (parts) {
-            console.log(parts[0] + highlight(parts[1]) + parts[2]);
-        }
-        next();
-    });
+    if (contentsRegex) {
+        walker.on("file", contentsSearchOnFile);
+        walker.on("directory", contentsSearchOnDirectory);
+    } else {
+        walker.on("file", filesystemSearchOnFile);
+        walker.on("directory", filesystemSearchOnDirectory);
+    }
 
-    walker.on("directory", function (root, dirStats, next) {
-        var absPath = path.join(root, dirStats.name);
-        var parts = regexHelpers.isMatch(absPath, pattern);
-        if (parts) {
-            console.log(parts[0] + highlight(parts[1]) + parts[2]);
-        }
-        next();
-    });
+
+}
+
+function filesystemSearchOnFile(root, fileStats, next) {
+    var absPath = path.join(root, fileStats.name);
+    var parts = regexHelpers.isMatch(absPath, filesystemRegex);
+    if (parts) {
+        highlightLine(parts);
+    }
+    next();
+
+}
+
+function contentsSearchOnFile(root, fileStats, next) {
+    var absPath = path.join(root, fileStats.name);
+    var parts = regexHelpers.isMatch(absPath, filesystemRegex);
+
+    if (parts) {
+        console.log("Searching contents of " + path.join(root, fileStats.name) );
+        // todo: Search contents for contentsRegex.
+    }
+
+    next();
+}
+
+function filesystemSearchOnDirectory(root, dirStats, next) {
+    var absPath = path.join(root, dirStats.name);
+    var parts = regexHelpers.isMatch(absPath, filesystemRegex);
+    if (parts) {
+        highlightLine(parts);
+    }
+    next();
+    
+    
+    
+}
+
+function contentsSearchOnDirectory(root, dirStats, next) {
+    next();
+}
+
+
+function highlightLine(parts) {
+    console.log(parts[0] + highlight(parts[1]) + parts[2]);
 }
